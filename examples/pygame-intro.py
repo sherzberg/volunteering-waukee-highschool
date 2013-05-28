b8c70d513310d6863a704e6b31e24fea9fbf9cb8
Spencer Herzberg <spencer.herzberg@gmail.com>
2013-05-27 20:27:56 -0500

    starting pygame examples

---
+++ examples/pygame-intro.py
@@ -0,0 +1,33 @@
+import pygame
+pygame.init()
+
+def main():
+    screen = pygame.display.set_mode((640, 480))
+
+    background = pygame.Surface(screen.get_size())
+    background.convert()
+    background.fill(pygame.color.Color('Yellow'))
+
+    running = True
+    clock = pygame.time.Clock()
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                print("Key pressed: %" % pygame.key.name(event.key))
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                line_start = pygame.mouse.get_pos()
+                pygame.draw.circle(background, (0, 0, 0), line_start, 5)
+            elif event.type == pygame.MOUSEBUTTONUP:
+                line_end = pygame.mouse.get_pos()
+                pygame.draw.line(background, (0, 0, 0), line_start, line_end, 3)
+
+        screen.blit(background, (0, 0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
